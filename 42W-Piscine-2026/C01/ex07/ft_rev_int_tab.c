3b9f24410ad39d1dcb26ed5fdebf8ef0c9bb7712
Dan Johnson <dajohnso@c1r6s3.42warsaw.pl>
2026-01-27 18:31:22 +0100

    Completed C01 ex07

--- 42W-Piscine-2026/C01/ex07/ft_rev_int_tab.c
+++ 42W-Piscine-2026/C01/ex07/ft_rev_int_tab.c
@@ -1,32 +1,41 @@
 void	ft_rev_int_tab(int *tab, int size)
 {
+	int	i;
+	int	j;
 	int	hold;
-	int	front;
-	int	back;
 
-	front = 0;
-	back = size - 1;
-	while (front != back)
+	i = 0;
+	j = size - 1;
+	while (i < j)
 	{
-		hold = tab[front];
-		tab[front] = tab[back];
-		tab[back] = hold;
-		front++;
-		back--; 
+		hold = tab[i];
+		tab[i] = tab[j];
+		tab[j] = hold;
+		i++;
+		j--;
 	}
 }
-
+/*
 #include <stdio.h>
 int	main(void)
 {
-	int	tab[6] = {1, 2, 3, 4, 5, 6};
+	int	tab[7] = {1, 2, 3, 4, 5, 6, 7};
+	int	i;
 
+	i = 0;
 	printf("%s", "The original array is: ");
-	while (*tab)
+	while (i < 7)
 	{
-		printf("%d ", *tab);
-		tab++;
+		printf("%d ", tab[i]);
+		i++;
 	}
-	ft_rev_int_tab(tab, 6);
-	printf("%s%d\n", "The reversed array is: ", *tab);
-}
+	ft_rev_int_tab(tab, 7);
+	printf("\n%s", "The reversed array is: ");
+	i = 0;
+	while (i < 7)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+}*/
