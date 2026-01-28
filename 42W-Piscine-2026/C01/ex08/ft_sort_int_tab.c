b0d4aa319f5146b5f398b2ee52ba2309debfe116
Dan Johnson <dajohnso@c2r5s4.42warsaw.pl>
2026-01-28 18:58:44 +0100

    Added ex08 to C01

---
+++ 42W-Piscine-2026/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,47 @@
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	max;
+	int i;
+
+	i = 0;
+	while (size > 0)
+	{
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				max = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = max;
+			}
+			i++;
+		}
+		size--;
+		i = 0;
+	}
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	tab[10] = {3, 7, 8, 2, 1, 9, 6, 5, 4, 10};
+	int	i;
+
+	i = 0;
+	printf("%s", "The unsorted array is: ");
+	while (i < 10)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+	ft_sort_int_tab(tab, 10);
+	i = 0;
+	printf("%s", "The array in ascending order is: ");
+	while (i < 10)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+}*/
