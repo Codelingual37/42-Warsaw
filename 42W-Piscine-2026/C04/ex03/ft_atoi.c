3e72155fb374254c663ef62e61268834fc0322e9
Dan Johnson <dajohnso@c3r16s4.42warsaw.pl>
2026-02-10 16:39:54 +0100

    Changed C04 ex03-Incomplete to ex03.

---
+++ 42W-Piscine-2026/C04/ex03/ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dajohnso <dajohnso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 09:57:06 by dajohnso          #+#    #+#             */
+/*   Updated: 2026/02/09 09:57:07 by dajohnso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+#include <stdio.h>
+#include "../ex02/ft_putnbr.c"
+
+void	ft_putnbr(int nb);
+
+int main(void)
+{
+	int result = ft_atoi(" ---+--+1234ab567");
+	//int result = ft_atoi("2147483647");
+	ft_putnbr(result);
+}*/
